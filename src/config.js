7cc8cc3826531d267a724328c057888e2a10971a
Sheheryar Nasir <sheheryar.nasir@technupur.com>
2024-01-25 18:00:07 +0500

    URL Change

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ if (
     // SERVER_URL: window.location.protocol + "//" + window.location.hostname + ":8989"
     // SERVER_URL: "http://165.232.187.214:2323"
     //SERVER_URL: 'http://54.176.250.244:8989',
-    // NEW_SERVER_URL: 'http://54.215.21.69:8989'
+    NEW_SERVER_URL: 'http://54.215.21.69:8989'
     // NEW_SERVER_URL: 'http://54.176.250.244:8989'
-    NEW_SERVER_URL: 'http://localhost:8989'
+    // NEW_SERVER_URL: 'http://localhost:8989'
   };
 }
 
